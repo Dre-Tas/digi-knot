b279987656cbce1046c6ddc48b8b95d426a12a20
Dhanjeet Sah <dhanjeetsyd@gmail.com>
2019-05-21 09:53:58 +1000

    Form Update

---
+++ DigiKnot/AssetInfo.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace DigiKnot
+{
+    partial class AssetInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AssetInfo));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.assetsDataSet = new DigiKnot.AssetsDataSet();
+            this.assetsDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assetsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assetsDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Asset Information";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(380, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Enter Asset ID and hit Search";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.assetsDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 119);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(594, 233);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // assetsDataSet
+            // 
+            this.assetsDataSet.DataSetName = "AssetsDataSet";
+            this.assetsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // assetsDataSetBindingSource
+            // 
+            this.assetsDataSetBindingSource.DataSource = this.assetsDataSet;
+            this.assetsDataSetBindingSource.Position = 0;
+            // 
+            // AssetInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 389);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AssetInfo";
+            this.Text = "AssetInfo";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assetsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assetsDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource assetsDataSetBindingSource;
+        private AssetsDataSet assetsDataSet;
+    }
+}
---
+++ DigiKnot/dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Asset_ID] VARCHAR (50) NOT NULL  ,
+    [GUID]     VARCHAR (50) NOT NULL ,
+    PRIMARY KEY CLUSTERED ([Asset_ID] ASC)
+);
+
